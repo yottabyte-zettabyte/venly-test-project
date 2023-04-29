a3825f84b773c1a794ca797068e8be424efbcaa4
Artem Zakomirnyi <artem@protopie.io>
2023-04-29 19:14:40 +0300

    Project base - III

---
+++ src/main/java/com/venly/testproject/config/ApplicationConfig.java
@@ -0,0 +1,19 @@
+package com.venly.testproject.config;
+
+import java.nio.charset.StandardCharsets;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        return messageSource;
+    }
+}
